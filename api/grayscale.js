406884f2b935a012f0600a724bc0bbe299720e7a
James-Lian <166276178+James-Lian@users.noreply.github.com>
2024-07-16 20:51:13 -0400

    Update grayscale.js

--- api/grayscale.js
+++ api/grayscale.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
     return res.status(200).end();
   }
 
-  const { url, width, height } = req.query;
+  const { url, width } = req.query;
 
   if (!url) {
     return res.status(400).json({ error: 'No URL provided' });
@@ -21,7 +21,9 @@ module.exports = async (req, res) => {
     const buffer = await response.buffer();
     const img = await loadImage(buffer);
 
-    const canvas = createCanvas(parseInt(width), parseInt(height));
+    const height = img.height * (width / img.width);
+
+    const canvas = createCanvas(parseInt(width), height);
     const context = canvas.getContext('2d');
     context.drawImage(img, 0, 0, canvas.width, canvas.height);
 
@@ -29,7 +31,7 @@ module.exports = async (req, res) => {
     const data = imageData.data;
 
     const grayscaleArray = new Array(canvas.height);
-    for (let y = 0; y < height; y++) {
+    for (let y = 0; y < canvas.height; y++) {
       grayscaleArray[y] = new Array(canvas.width);
     }
   
